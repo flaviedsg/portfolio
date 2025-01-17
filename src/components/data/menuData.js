56fd331590b87fcaf87be698afd2a5ad4e9c595d
Flavie Dussauge <flavie.dussauge@dijonformation.com>
2025-01-17 15:47:42 +0100

    validation

--- src/components/data/menuData.js
+++ src/components/data/menuData.js
@@ -1,11 +1,12 @@
 export const mainMenu = [
-     {link:"#", content:"Home", icon:"lucide:home"},
-     {link:"#about", content:"About", icon:"octicon:person-24"},
-     {link:"#portafolio", content:"Portfolio", icon:"carbon:portfolio"},
-     {link:"#blog", content:"Blog", icon:"mdi:blog"}
+     {link:"#", content:"Acceuil", icon:"lucide:home"},
+     {link:"#about", content:"À propos", icon:"octicon:person-24"},
+     {link:"#skills", content:"Compétences", icon:"skills"},
+     {link:"#portafolio", content:"Projet", icon:"carbon:portfolio"},
+     {link:"#blog", content:"Veille technologique", icon:"mdi:blog"}
 ]
 export const menuBlog = [
-     {link:"/", content:"Home", icon:"lucide:home"},
-     {link:"/#about", content:"About", icon:"octicon:person-24"},
-     {link:"/#portafolio", content:"Portfolio", icon:"carbon:portfolio"},
+     {link:"/", content:"Acceuil", icon:"lucide:home"},
+     {link:"/#about", content:"À propos", icon:"octicon:person-24"},
+     {link:"/#portafolio", content:"Projet", icon:"carbon:portfolio"},
 ]
--- src/components/data/portfolioData.js
+++ src/components/data/portfolioData.js
@@ -17,30 +17,41 @@
  */
 export const portafolioData = [
 	{
-		imgSrc: 'https://dt2sdf0db8zob.cloudfront.net/wp-content/uploads/2021/06/copy-of-the-6-best-restaurant-website-templates-1.png',
-		title: 'Restaurant',
+		imgSrc: 'image.png',
+		title: 'Power BI trains partant en retards',
 		skills: ['React', 'StyledComponents'],
 		descripcion:
-			'Laboris ex laboris mollit esse fugiat aute cillum nostrud enim dolor sit. Reprehenderit et non nulla irure aute nostrud commodo aute.',
+		"J'ai créé un tableau de bord Power BI concernant les trains partant en retards, ce qui permet d'avoir une analyse direct et rapide des trains partant en retards.",
 		demoURL: '',
 		repoURL: '',
 		anim: 'fade-right',
 	},
 	{
-		imgSrc: 'https://fiverr-res.cloudinary.com/images/q_auto,f_auto/gigs/299377097/original/7eb7dcebe244fcf5ad75d92b0969fc116946bd57/create-professional-amd-responsive-wordpress-website.jpg',
-		title: 'E-commerce',
+		imgSrc: 'contrôleur.png',
+		title: 'Power BI\n pour le rapport mensuel des contrôleurs',
 		skills: ['JavaScript', 'Bootstrap'],
 		descripcion:
-			' Laboris ex laboris mollit esse fugiat aute cillum nostrud enim dolor sit. Reprehenderit et non nulla irure aute nostrud commodo aute.',
+			" J'ai mis en place un tableau de bord Power BI pour le rapport mensuel des contrôleurs, ce qui permet d’avoir une analyse direct et rapide des resulats contrôleurs.",
 		demoURL: '',
 		repoURL: '',
 		anim: 'fade-up',
 		averageBrightness: 0.1,
 	},
 	{
-		imgSrc: 'https://i0.wp.com/themes.svn.wordpress.org/cafe-cafeteria/1.5.3/screenshot.png',
-		title: 'Coffe Store',
+		imgSrc: 'Capture d’écran 2024-12-19 134624.png',
+		title: 'Création d\'un site Le bon koin',
 		skills: ['React', 'CSS'],
+		descripcion:
+			"Création d'un site web pour mon parcours scolaire. Ce site est un site similaire au bon coin, il permet de vendre et d'acheter des produits.",
+		demoURL: '',
+		repoURL: '',
+		anim: 'fade-left',
+		
+	},
+	{
+		imgSrc: 'black-background.png',
+		title: 'Création d\'une applcation pour téléphone',
+		skills: ['Power BI', 'Excel'],
 		descripcion:
 			'Laboris ex laboris mollit esse fugiat aute cillum nostrud enim dolor sit. Reprehenderit et non nulla irure aute nostrud commodo aute.Laboris ex laboris mollit esse fugiat aute cillum nostrud enim dolor sit. Reprehenderit et non nulla irure aute nostrud commodo aute.Laboris ex laboris mollit esse fugiat aute cillum nostrud enim dolor sit. Reprehenderit et non nulla irure aute nostrud commodo aute.',
 		demoURL: '',
@@ -48,35 +59,16 @@ export const portafolioData = [
 		anim: 'fade-left',
 	},
 	{
-		imgSrc: 'https://www.corse.mx/cdn/shop/files/tienda_lenceria-lenceria-hot_sale-ofertas-mexico-lenceria_seyx-encaje-corse_mx-corse_lenceria_1200_x_560_px_13.png?v=1712270274&width=1500',
-		title: 'Coffe Store',
-		skills: ['React', 'CSS'],
+		imgSrc: 'User_Assistance_Illustration.png',
+		title: 'Asssistance informatique à mes collègues',
+		skills: [],
 		descripcion:
 			'Laboris ex laboris mollit esse fugiat aute cillum nostrud enim dolor sit. Reprehenderit et non nulla irure aute nostrud commodo aute.Laboris ex laboris mollit esse fugiat aute cillum nostrud enim dolor sit. Reprehenderit et non nulla irure aute nostrud commodo aute.Laboris ex laboris mollit esse fugiat aute cillum nostrud enim dolor sit. Reprehenderit et non nulla irure aute nostrud commodo aute.',
 		demoURL: '',
 		repoURL: '',
 		anim: 'fade-left',
 	},
-	{
-		imgSrc: 'https://i0.wp.com/themes.svn.wordpress.org/cafe-cafeteria/1.5.3/screenshot.png',
-		title: 'Coffe Store',
-		skills: ['React', 'CSS'],
-		descripcion:
-			'Laboris ex laboris mollit esse fugiat aute cillum nostrud enim dolor sit. Reprehenderit et non nulla irure aute nostrud commodo aute.Laboris ex laboris mollit esse fugiat aute cillum nostrud enim dolor sit. Reprehenderit et non nulla irure aute nostrud commodo aute.Laboris ex laboris mollit esse fugiat aute cillum nostrud enim dolor sit. Reprehenderit et non nulla irure aute nostrud commodo aute.',
-		demoURL: '',
-		repoURL: '',
-		anim: 'fade-left',
-	},
-	{
-		imgSrc: 'https://dt2sdf0db8zob.cloudfront.net/wp-content/uploads/2021/06/copy-of-the-6-best-restaurant-website-templates-1.png',
-		title: 'Restaurant',
-		skills: ['React', 'StyledComponents'],
-		descripcion:
-			'Laboris ex laboris mollit esse fugiat aute cillum nostrud enim dolor sit. Reprehenderit et non nulla irure aute nostrud commodo aute.',
-		demoURL: '',
-		repoURL: '',
-		anim: 'fade-right',
-	}
+
 ];
 
 const skillIcons = {
